0512b4159d7365838a8b011885972c45ab4d7e05
Gustavo Nascimento <gusta.nas@gmail.com>
2018-08-28 10:12:56 +0200

    hover effect on projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Project.css'
 
+
 const Project = props => (
 	<div className="Project">
 		<h3><Link to={props.link} target="_blank">{props.title}</Link></h3>
-		<Link to={props.link} target="_blank"><img src={props.image}/></Link>
+		<div className="Image">
+			<Link to={props.link} target="_blank"><img src={props.image}/></Link>
+			<h1>{props.number}</h1>
+		</div>
 	</div>
 )
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,10 +15,10 @@ const ProjectsPage = () => (
         </div>
 
         <div className="Projects">
-          <Project title="#04 Sentiance" image="https://cl.ly/3dbf8e4a45a3/Sentiance.png" link="/Sentiance"/>
-          <Project title="#03 Bookizy" image="https://cl.ly/149f92fd80df/Bookizy.png" link="/Bookizy"/>
-          <Project title="#02 Immoweb" image="https://cl.ly/e69d2f9922d0/Immoweb.jpg" link="/Immoweb"/>
-          <Project title="#01 Dilemme" image="https://cl.ly/bf6d89c19bd8/Dilemme.png" link="/Dilemme"/>
+          <Project title="#04 Sentiance" number = "#04" image="https://cl.ly/3dbf8e4a45a3/Sentiance.png" link="/Sentiance"/>
+          <Project title="#03 Bookizy" number = "#03" image="https://cl.ly/149f92fd80df/Bookizy.png" link="/Bookizy"/>
+          <Project title="#02 Immoweb" number = "#02" image="https://cl.ly/e69d2f9922d0/Immoweb.jpg" link="/Immoweb"/>
+          <Project title="#01 Dilemme" number = "#01" image="https://cl.ly/bf6d89c19bd8/Dilemme.png" link="/Dilemme"/>
         </div>
     </div>
   </div>
