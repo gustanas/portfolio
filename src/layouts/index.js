78b587b9f1cb72b0ec898a2ca64a765ef638682c
Gustavo Nascimento <gusta.nas@gmail.com>
2018-08-29 09:39:18 +0200

    add GA

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,13 +10,25 @@ import './projectDetails.css'
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: "Hi there! I'm Gustavo, I design and build iOS apps." },
-        { name: 'keywords', content: 'iOS, developer, Swift, Objective-C, Cocoa, Xcode, Gustavo, Nascimento' },
-      ]}
-    />
+    <Helmet>
+    <title>{data.site.siteMetadata.title}</title>
+    <meta name="description" content="Hi there! I'm Gustavo, I design and build iOS apps." />
+    <meta name="keywords" content="iOS, developer, Swift, Objective-C, Cocoa, Xcode, Gustavo, Nascimento" />
+
+    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-79159071-2"></script>
+    <script>
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-79159071-2');
+    `}
+    </script>
+
+
+    </Helmet>
     {children()}
   </div>
 )
