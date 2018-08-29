619149421345c5c9e2d05f7d2e269697fcea261b
Gustavo Nascimento <gusta.nas@gmail.com>
2018-08-29 09:12:10 +0200

    add description in metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Gustavo Nascimento - iOS Developer',
+    siteUrl: `https://www.gustavonascimento.com`,
+    description: `Hi there! I'm Gustavo, I design and build iOS apps.`,
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
