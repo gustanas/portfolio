0625222339102a64de48f9019d3a6b7539be12f5
Gustavo Nascimento <gustavo.nascimento@novemberfive.co>
2018-12-05 21:02:24 +0100

    add strongify

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,7 +15,7 @@ const ProjectsPage = () => (
         </div>
 
         <div className="Projects">
-          <Project title="#05 Strongify" number = "#05" image="https://cl.ly/bf6d89c19bd8/Dilemme.png" link="/Strongify"/>
+          <Project title="#05 Strongify" number = "#05" image="https://duaw26jehqd4r.cloudfront.net/items/3W1J2Z2j2d1X253f3n01/Strongify.png" link="/Strongify"/>
           <Project title="#04 Sentiance" number = "#04" image="https://cl.ly/3dbf8e4a45a3/Sentiance.png" link="/Sentiance"/>
           <Project title="#03 Bookizy" number = "#03" image="https://cl.ly/149f92fd80df/Bookizy.png" link="/Bookizy"/>
           <Project title="#02 Immoweb" number = "#02" image="https://cl.ly/e69d2f9922d0/Immoweb.jpg" link="/Immoweb"/>
